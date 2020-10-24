fe32bc44764ea69a5d725adc12f71680faea68f9
Tomas Diez <ltomasdiez@gmail.com>
2020-10-24 04:32:32 -0300

    JavaScriptTask:01-Solved

---
+++ Task1/Task1.js
@@ -0,0 +1,30 @@
+/*
+*****************************Variables y Operadores****************************
+*a)Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma
+de ambos números en una 3er variable.
+*b)Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+*c)Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras
+del string) guardando el resultado la suma una 3er variable (utilizar length).
+*/
+
+/* EJERCICIO A */
+var numUno = 2;
+var numDos = 3;
+var suma = numUno+numDos;
+console.log(suma);
+
+/* EJERCICIO B */
+var stringUno = 'Hola ';
+var stringDos = 'Mundo';
+var stringTres = stringUno+stringDos;
+console.log(stringTres)
+
+/* EJERCICIO C */
+var stringCuatro = 'Hola Mundo!'
+var stringCinco = ' Me gusta JS'
+var stringLargo = stringCuatro.length+stringCinco.length;
+console.log(stringLargo);
+
+
+
+
